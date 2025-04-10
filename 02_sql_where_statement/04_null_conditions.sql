a3f1229a9f4bb7364a67da7b89550a742dc1bf1f
danilsyah <danilsyaharihardjo@gmail.com>
2025-04-10 10:38:54 +0700

    is null conditions

---
+++ 02_sql_where_statement/04_null_conditions.sql
@@ -0,0 +1,15 @@
+/*
+Operator IS NULL digunakan untuk memfiter data yang bernilai null. Contoh kasusnya, saya mau menampilkan data karywan yang tidak memiliki manager. Berikut querynya:
+*/
+
+select EMPLOYEE_ID, first_name, salary, job_id, MANAGER_ID
+from EMPLOYEES
+where manager_id is null;
+
+SELECT EMPLOYEE_ID, FIRST_NAME, EMAIL
+from EMPLOYEES
+where EMAIL is null;
+
+select COUNT(EMPLOYEE_ID) as jumlah
+from EMPLOYEES
+WHERE COMMISSION_PCT is null;
